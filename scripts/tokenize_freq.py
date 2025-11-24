af10f6862f59f176628aa7abcb7dacb1a108a9e8
WANG Xiaobo <sibelwang17@gmail.com>
2025-11-24 16:26:03 +0100

    ajout d'une freqtop80

--- scripts/tokenize_freq.py
+++ scripts/tokenize_freq.py
@@ -5,13 +5,13 @@ from pathlib import Path
 
 import jieba 
 
-CLEAN_PATH = Path("data/processed/corpus_cn_clean.txt")
-FREQ_PATH = Path("data/processed/freq_top50.csv")
+CLEAN_PATH = Path("/home/sibel/lpt-metarecit/data/processed/corpus_clean.json")
+FREQ_PATH = Path("/home/sibel/lpt-metarecit/data/processed/freq_top80.csv")
 
 STOPWORDS = {
-    "的", "了", "和", "与", "及", "在", "对", "为", "是", "着", "也", "都",
-    "我们", "他们", "这些", "那些", "一个", "这个", "那个",
-    "通过", "更加", "进一步",
+    "的", "了", "和", "与", "及", "在", "对", "为", "是", "着", "也", "都", "同", "有", "好", "并", "10", "08","cn", "n1", "html",
+    "我们", "他们", "这些", "那些", "一个", "这个", "那个", "title", "full", "从", "到", "让", "下", "url", "http", "cpc",
+    "通过", "更加", "更", "年", "月", "中", "以", "等", "日", "将", "要", "上", "有", "多", "大", "people", "com", "summary"
 }
 
 def tokenize(text: str) -> list[str]:
@@ -36,15 +36,15 @@ def main():
     tokens = filter_tokens(tokens)
 
     freq = Counter(tokens)
-    top50 = freq.most_common(50)
+    top80 = freq.most_common(80)
 
     print(f"nb de tokens filtre{len(tokens)}")
-    for w, c in top50:
+    for w, c in top80:
         print(f"{w}\t{c}")
 
     with FREQ_PATH.open("w", encoding="utf-8") as f:
         f.write("token,freq\n")
-        for w, c in top50:
+        for w, c in top80:
             w_clean = w.replace(",", "，")
             f.write(f"{w_clean},{c}\n")
 
